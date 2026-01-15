f29ac6e80278180f88f0527295a4dfb7c9b6b775
Dyck.lopez <dyck.lopez@correo.policia.gov.co>
2026-01-15 12:43:18 -0500

    ajuste modal soporte y politicas de uso

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,11 +1,94 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
-export class FooterComponent {}
+export class FooterComponent implements OnDestroy {
+
+  supportOpen = false;
+
+  supportForm = {
+    tipo: 'Incidente (error)',
+    prioridad: 'Media',
+    asunto: '',
+    descripcion: '',
+    incluirDatosTecnicos: true,
+    adjuntos: [] as File[]
+  };
+
+
+  openSupport(): void {
+    this.supportOpen = true;
+    document.body.classList.add('ui-modal-open');
+  }
+
+
+  closeSupport(): void {
+    this.supportOpen = false;
+    document.body.classList.remove('ui-modal-open');
+  }
+
+  onFilesSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    const files = input.files ? Array.from(input.files) : [];
+    this.supportForm.adjuntos = files;
+  }
+
+  submitSupport(): void {
+    if (!this.supportForm.asunto.trim() || !this.supportForm.descripcion.trim()) {
+      alert('Completa Asunto y Descripción.');
+      return;
+    }
+
+    console.log('Ticket (solo UI):', this.supportForm);
+
+    this.resetSupportForm();
+
+    this.closeSupport();
+  }
+
+
+  resetSupportForm(): void {
+    this.supportForm = {
+      tipo: 'Incidente (error)',
+      prioridad: 'Media',
+      asunto: '',
+      descripcion: '',
+      incluirDatosTecnicos: true,
+      adjuntos: []
+    };
+  }
+  policyOpen = false;
+
+openPolicy(): void {
+  this.policyOpen = true;
+  document.body.classList.add('ui-modal-open');
+}
+
+closePolicy(): void {
+  this.policyOpen = false;
+
+  if (!this.supportOpen) {
+    document.body.classList.remove('ui-modal-open');
+  }
+}
+
+
+ 
+  @HostListener('document:keydown.escape')
+  onEsc(): void {
+    if (this.supportOpen) {
+      this.closeSupport();
+    }
+  }
+
+  ngOnDestroy(): void {
+    document.body.classList.remove('ui-modal-open');
+  }
+}
