b926ad35ec0a8d64ca9c8cda2661ff31226e9a87
WILSON ANDRES LOPEZ DE ALBA <wilson.lopezd@correo.policia.gov.co>
2026-03-06 08:23:39 -0500

    sincronizae

--- backend/oftic/oftic.dl/obj/Debug/net8.0/Datos.AssemblyInfo.cs
+++ backend/oftic/oftic.dl/obj/Debug/net8.0/Datos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Datos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea4eeefc02852c501d0b1c01ab12bdce5df6242c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a717feb12eecea9d4823253eaae2de7e88d1c17f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Datos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Datos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- backend/oftic/oftic/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ backend/oftic/oftic/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f978c8ec6a7fcd988b01bd1f70898510a5dce3c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a717feb12eecea9d4823253eaae2de7e88d1c17f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
